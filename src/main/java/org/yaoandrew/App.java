caf22483a02955fef51cc21d7727aab551607424
yaoandrew <1547809+yaoandrew@users.noreply.github.com>
2017-11-29 15:58:05 -0800

    Clean up exception handling

--- src/main/java/org/yaoandrew/App.java
+++ src/main/java/org/yaoandrew/App.java
@@ -7,31 +7,20 @@ import java.net.Socket;
 public class App {
 
     static final int DEFAULT_PORT = 5000;
+    static final String HTTP_HEADER = ""
     static ServerSocket server;
 
     public static void main( String[] args ) {
-
         try {
 
             server = new ServerSocket(DEFAULT_PORT);
-
-        } catch (IOException e) {
-
-            System.out.println(e);
-
-        }
-
-        System.out.println("Server started...");
-
-        try {
+            System.out.println("Server started...");
 
             Socket client = server.accept();
+            System.out.println("Client connected");
 
         } catch (IOException e) {
-
             System.out.println(e);
         }
-
-        System.out.println("Client connected");
     }
 }
