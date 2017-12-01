406c5dfda438b0f867f0254a201b737e179504a7
yaoandrew <1547809+yaoandrew@users.noreply.github.com>
2017-11-30 20:30:07 -0800

    Write to socket

--- src/main/java/org/yaoandrew/App.java
+++ src/main/java/org/yaoandrew/App.java
@@ -1,23 +1,35 @@
 package org.yaoandrew;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.Principal;
 
 public class App {
 
     static final int DEFAULT_PORT = 5000;
-    static final String HTTP_HEADER = ""
-    static ServerSocket server;
+    static final String HTTP_HEADER = "";
+    static final String GET_RESPONSE = "";
+    static ServerSocket serverSocket;
 
     public static void main( String[] args ) {
         try {
 
-            server = new ServerSocket(DEFAULT_PORT);
+            serverSocket = new ServerSocket(DEFAULT_PORT);
             System.out.println("Server started...");
 
-            Socket client = server.accept();
+            Socket clientSocket = serverSocket.accept();
             System.out.println("Client connected");
+
+            InputStreamReader reader = new InputStreamReader(clientSocket.getInputStream());
+            System.out.println(reader);
+
+            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            writer.println("HTTP/1.1 OK 200");
+
 
         } catch (IOException e) {
             System.out.println(e);
