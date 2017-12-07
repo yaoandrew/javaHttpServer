028bd919a36e79ac8ab0c0e587974708681e91a5
yaoandrew <1547809+yaoandrew@users.noreply.github.com>
2017-12-07 10:03:01 -0800

    Add methods to Response object

--- src/main/java/org/yaoandrew/ClientHandler.java
+++ src/main/java/org/yaoandrew/ClientHandler.java
@@ -6,11 +6,11 @@ import java.net.Socket;
 class ClientHandler implements Runnable {
     Socket client;
 
-    ClientHandler (Socket client) {
+    ClientHandler(Socket client) {
         this.client = client;
     }
 
-    public void run () {
+    public void run() {
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
@@ -18,12 +18,9 @@ class ClientHandler implements Runnable {
             System.out.println(reader.readLine());
 
             writer.write(Response.getStatusLine());
-            writer.newLine();
-            writer.write("Server: Yao1.0");
-            writer.newLine();
-            writer.newLine();
-            writer.write("Hello World");
-            writer.newLine();
+            writer.write(Response.getHeaders());
+            writer.write(Response.getSeperator());
+            writer.write(Response.getNewLine());
             System.out.println("Response sent");
             writer.close();
             reader.close();
--- src/main/java/org/yaoandrew/Response.java
+++ src/main/java/org/yaoandrew/Response.java
@@ -2,9 +2,29 @@ package org.yaoandrew;
 
 public class Response {
 
-    private static String statusLine = "HTTP/1.1 200 OK";
+    private static String statusLine = "HTTP/1.1 200 OK\r\n";
+    private static String headers = "Allow: GET,OPTIONS\r\n";
+    private static String body = "";
+    private static String newLine = "\r\n";
 
-    public static String getStatusLine() {
+
+    static String getStatusLine() {
         return statusLine;
+    }
+
+    static String getHeaders() {
+        return headers;
+    }
+
+    static String getSeperator() {
+        return newLine + newLine;
+    }
+
+    static String getNewLine() {
+        return newLine;
+    }
+
+    static String getBody() {
+        return body;
     }
 }
