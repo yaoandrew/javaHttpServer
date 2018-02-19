16e8a2a015cfa97633d1975488c3cd78756770f5
yaoandrew <1547809+yaoandrew@users.noreply.github.com>
2018-02-19 13:38:43 -0800

    Adds error checking for range in Partial Handler

--- src/test/java/handlers/PartialContentHandlerTest.java
+++ src/test/java/handlers/PartialContentHandlerTest.java
@@ -34,36 +34,54 @@ public class PartialContentHandlerTest {
   @Test
   public void PartialContentHandlerCanReturnFullRangeGiven() throws IOException {
 
-    String rawRequest = "GET /partial_content.txt HTTP/1.1\r\nRange: 0-4\r\n";
+    String rawRequest = "GET /partial_content.txt HTTP/1.1\r\nRange: bytes=0-4\r\n";
     File file = temporaryFolder.newFile("partial_content.txt");
     String content = "This is a file that contains text to read part of in order to fulfill a 206. ";
     Files.write(file.toPath(), content.getBytes());
     PartialContentHandler partialContentHandler = new PartialContentHandler(file);
 
-    assertThat(Arrays.copyOfRange(content.getBytes(), 0, 5), equalTo(partialContentHandler.getResponse(parser.parse(rawRequest)).getBody()));
+    assertThat(partialContentHandler.getResponse(parser.parse(rawRequest)).getBody(),
+        equalTo(Arrays.copyOfRange(content.getBytes(), 0, 5)));
   }
 
   @Test
   public void PartialContentHandlerCanReturnEndRangeGiven() throws IOException {
 
-    String rawRequest = "GET /partial_content.txt HTTP/1.1\r\nRange: -6\r\n";
+    String rawRequest = "GET /partial_content.txt HTTP/1.1\r\nRange: bytes=-6\r\n";
     File file = temporaryFolder.newFile("partial_content.txt");
     String content = "This is a file that contains text to read part of in order to fulfill a 206. ";
     Files.write(file.toPath(), content.getBytes());
     PartialContentHandler partialContentHandler = new PartialContentHandler(file);
 
-    assertThat(Arrays.copyOfRange(content.getBytes(), 71, 77), equalTo(partialContentHandler.getResponse(parser.parse(rawRequest)).getBody()));
+    assertThat(partialContentHandler.getResponse(parser.parse(rawRequest)).getBody(),
+        equalTo(Arrays.copyOfRange(content.getBytes(), 71, 77)));
   }
 
   @Test
   public void PartialContentHandlerCanReturnBeginRangeGiven() throws IOException {
 
-    String rawRequest = "GET /partial_content.txt HTTP/1.1\r\nRange: 4-\r\n";
+    String rawRequest = "GET /partial_content.txt HTTP/1.1\r\nRange: bytes=4-\r\n";
     File file = temporaryFolder.newFile("partial_content.txt");
     String content = "This is a file that contains text to read part of in order to fulfill a 206. ";
     Files.write(file.toPath(), content.getBytes());
     PartialContentHandler partialContentHandler = new PartialContentHandler(file);
 
-    assertThat(Arrays.copyOfRange(content.getBytes(), 4, 77), equalTo(partialContentHandler.getResponse(parser.parse(rawRequest)).getBody()));
+    assertThat(partialContentHandler.getResponse(parser.parse(rawRequest)).getBody(),
+      equalTo(Arrays.copyOfRange(content.getBytes(), 4, 77)));
   }
+
+  @Test
+  public void PartialContentHandlerCanHandleBadRangeInput() throws IOException {
+
+    String rawRequest = "GET /partial_content.txt HTTP/1.1\r\nRange: bytes=4-88\r\n";
+    File file = temporaryFolder.newFile("partial_content.txt");
+    String content = "This is a file that contains text to read part of in order to fulfill a 206. ";
+    Files.write(file.toPath(), content.getBytes());
+    PartialContentHandler partialContentHandler = new PartialContentHandler(file);
+
+    assertThat(partialContentHandler.getResponse((parser.parse(rawRequest))).getStatusLine(),
+      equalTo("HTTP/1.1 416 Range Not Satisfiable"));
+
+  }
+
 }
