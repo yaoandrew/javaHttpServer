f9bbea54ba1a7087b947885e8ea28f1c26f31ce8
yaoandrew <1547809+yaoandrew@users.noreply.github.com>
2018-02-28 18:28:22 -0800

    Clean up Router class

--- src/main/java/router/Router.java
+++ src/main/java/router/Router.java
@@ -41,8 +41,8 @@ public class Router {
     if (!routeExistsInMap(request.getSimpleUri())) {
 
       File file = new File(serverDir + request.getSimpleUri());
-      System.out.println("This is the path: " + file.toString());
-      System.out.println("Does the path exist? " + file.exists());
+      //System.out.println("This is the path: " + file.toString());
+      //System.out.println("Does the path exist? " + file.exists());
 
       if(isPartialContentRequest(request) & isValidPathAndFile(file)){
         return new PartialContentHandler(file);
