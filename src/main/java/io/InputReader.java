e3b140841d463c4a3156459b2a5b24e24f838a57
yaoandrew <1547809+yaoandrew@users.noreply.github.com>
2018-03-08 14:44:31 -0800

    Move catching of IOException closer to InputStream and Reader, clean up input parts of run method

--- src/main/java/io/InputReader.java
+++ src/main/java/io/InputReader.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
 
 public class InputReader {
 
@@ -33,6 +32,10 @@ public class InputReader {
     }
 
     return rawRequest;
+  }
+
+  public void close() throws IOException {
+    bufferedReader.close();
   }
 
 }
