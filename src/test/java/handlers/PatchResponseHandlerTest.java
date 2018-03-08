3d570226ccd004ce7cd9b709c02905bece80145d
Andrew J Yao <1547809+yaoandrew@users.noreply.github.com>
2018-03-08 00:33:36 -0800

    Refactor patch test, add sha1 encoder

--- src/test/java/handlers/PatchResponseHandlerTest.java
+++ src/test/java/handlers/PatchResponseHandlerTest.java
@@ -1,6 +1,5 @@
 package handlers;
 
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
@@ -10,7 +9,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import messages.HTTPStatus;
 import messages.Response;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -21,10 +19,11 @@ public class PatchResponseHandlerTest {
   @Rule
   public TemporaryFolder tempFolder = new TemporaryFolder();
 
+  RequestParser parser = new RequestParser();
+
   @Test
   public void GetReturnsStatus200() throws IOException {
     String requestString = "GET /patch-content.txt HTTP/1.1";
-    RequestParser parser = new RequestParser();
     File serverFile = tempFolder.newFile("patch-content.txt");
 
     FileSystemHandler fileSystemHandler = new FileSystemHandler(serverFile);
@@ -35,10 +34,9 @@ public class PatchResponseHandlerTest {
 
   @Test
   public void PatchReturnsStatus204() throws IOException {
-    String sha1Hash = "dc50a0d27dda2eee9f65644cd7e4c9cf11de8bec";
-    String requestString = "PATCH /patch-content.txt HTTP/1.1\r\nIf-Match: " + sha1Hash +
+    String defaultContentSha1Hash = "dc50a0d27dda2eee9f65644cd7e4c9cf11de8bec";
+    String requestString = "PATCH /patch-content.txt HTTP/1.1\r\nIf-Match: " + defaultContentSha1Hash +
       "\r\n\r\npatched content";
-    RequestParser parser = new RequestParser();
     File serverFile = tempFolder.newFile("patch-content.txt");
     FileWriter fileWriter = new FileWriter(serverFile);
     fileWriter.write("default content");
@@ -52,10 +50,9 @@ public class PatchResponseHandlerTest {
 
   @Test
   public void PatchChangesResourceIfEtagMatches() throws IOException {
-    String sha1Hash = "dc50a0d27dda2eee9f65644cd7e4c9cf11de8bec";
+    String defaultContentSha1Hash = "dc50a0d27dda2eee9f65644cd7e4c9cf11de8bec";
     String requestString  = "PATCH /patch-content.txt HTTP/1.1\r\nIf-Match: " +
-        sha1Hash + "\r\n\r\npatched content";
-    RequestParser parser = new RequestParser();
+        defaultContentSha1Hash + "\r\n\r\npatched content";
     File serverFile = tempFolder.newFile("patch-content.txt");
     FileWriter fileWriter = new FileWriter(serverFile);
     fileWriter.write("default content");
