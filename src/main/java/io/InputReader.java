7eb9e20fef739e42d67be65a14eb66ffbac77d3c
yaoandrew <1547809+yaoandrew@users.noreply.github.com>
2018-03-08 11:41:36 -0800

    Create basic reader that takes an inputstream and can return request line

--- src/main/java/io/InputReader.java
+++ src/main/java/io/InputReader.java
@@ -1,10 +1,24 @@
 package io;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
 
 public class InputReader {
 
-  BufferedReader reader;
-  String rawRequest;
+  private InputStream inputStream;
+
+  public InputReader (InputStream inputStream) {
+    this.inputStream = inputStream;
+  }
+
+  public String readLine() throws IOException {
+    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+    return bufferedReader.readLine();
+  }
 
 }
--- src/test/java/io/InputReaderTest.java
+++ src/test/java/io/InputReaderTest.java
@@ -2,42 +2,22 @@ package io;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class InputReaderTest {
 
   private String basicRequest = "GET /form HTTP/1.1";
   private String requestWithHeaders = "GET /form HTTP/1.1\r\nAccept: */*\r\nContent-Length: 3000\r\n\r\n";
-  private InputReader testInputReader = new InputReader();
-
 
   @Test
   public void InputReaderReadsBasicRequest() throws IOException {
 
     ByteArrayInputStream inputStream = new ByteArrayInputStream(basicRequest.getBytes());
-    InputStreamReader isr = new InputStreamReader(inputStream);
-    BufferedReader br = new BufferedReader(isr);
-    testInputReader.reader = br;
+    InputReader reader = new InputReader(inputStream);
 
-    assertEquals(testInputReader.reader.readLine() , basicRequest);
+    assertEquals(reader.readLine(), basicRequest);
   }
-
-  @Ignore
-  public void InputReaderReadsRequestWithHeaders() throws IOException {
-
-    ByteArrayInputStream inputStream = new ByteArrayInputStream(requestWithHeaders.getBytes());
-    InputStreamReader isr = new InputStreamReader(inputStream);
-    BufferedReader br = new BufferedReader(isr);
-    testInputReader.reader = br;
-
-    assertEquals(testInputReader.reader.readLine() , requestWithHeaders);
-  }
-
-
 
 }
